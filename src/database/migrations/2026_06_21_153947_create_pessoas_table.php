0a6bd1e4131c84649364079f9392bdb1d887e010
Wesley Santos <wesleysantossilva.2019.2@gmail.com>
2026-06-21 13:21:32 -0300

    Update pessoas migration

--- src/database/migrations/2026_06_21_153947_create_pessoas_table.php
+++ src/database/migrations/2026_06_21_153947_create_pessoas_table.php
@@ -12,10 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('pessoas', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); 
             $table->string('nome');
             $table->string('sobrenome');
+            $table->timestamps();
+           
         });
     }
 
