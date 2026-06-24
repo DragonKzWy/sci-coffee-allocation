0e448987b8693ad17615b59882ae4d35c08620e4
Wesley Santos <wesleysantossilva.2019.2@gmail.com>
2026-06-24 17:38:25 -0300

    Extend shared navigation menu with allocation links

--- src/resources/views/menu.blade.php
+++ src/resources/views/menu.blade.php
@@ -2,5 +2,7 @@
     <a href="/">Home</a> |
     <a href="/pessoas">Pessoas</a> |
     <a href="/salas">Salas</a> |
-    <a href="/espacos-cafe">Espaços de Café</a>
+    <a href="/espacos-cafe">Espaços de Café</a> |
+    <a href="/alocacoes-sala">Alocações de Sala</a> |
+    <a href="/alocacoes-cafe">Alocações de Café</a>
 </p>
